61b20b8a4e4a48185cea9b239a6156e1aed659a5
Mark Dietzer <git@doridian.net>
2021-05-28 16:43:21 -0700

    Basic tape mgmt

---
+++ drive.py
@@ -0,0 +1,40 @@
+from subprocess import call
+
+class Drive():
+    def __init__(self, dev):
+        self.dev = '/dev/%s' % dev
+        self.mountpoint = None
+
+        fh = open('/sys/class/scsi_tape/%s/device/generic/dev' % dev, 'r')
+        fd = fh.read().strip()
+        fh.close()
+        self.sgid = fd.split(':')[1]
+
+    def set_encryption(self, on):
+        call(['stenc', '-f', self.dev, '-e', 'on' if on else 'off', '-k', '/mnt/keydisk/tape.key', '-a', '1', '--ckod'])
+
+    def eject(self):
+        self.unmount()
+        call(['/opt/tape/TapeTool.sh', 'eject', self.sgid])
+
+    def load(self):
+        self.unmount()
+        call(['/opt/tape/TapeTool.sh', 'load', self.sgid])
+
+    def init(self, barcode, name):
+        self.unmount()
+        self.set_encryption(True)
+        call(['mkltfs', '--device=%s' % self.dev, '-s', barcode, '-n', name, '-f'])
+
+    def mount(self, mountpoint='/mnt/tape'):
+        self.unmount()
+        self.set_encryption(True)
+        self.mountpoint = mountpoint
+        call(['ltfs', '-o', 'eject', '-o', 'sync_type=unmount', mountpoint])
+
+    def unmount(self):
+        if self.mountpoint is None:
+            return
+        call(['umount', self.mountpoint])
+        self.set_encryption(False)
+        self.mountpoint = None
