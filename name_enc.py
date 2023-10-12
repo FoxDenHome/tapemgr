0dc5f808b797c3ace412b9cc0a12ba6369b04a00
Doridian <git@doridian.net>
2023-10-11 22:35:42 -0700

    fix this crap

--- name_enc.py
+++ name_enc.py
@@ -1,25 +1,31 @@
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 from Crypto.Cipher import AES
+from typing import Any
 
 class NameCryptor:
     def __init__(self, key: bytes):
         super().__init__()
-        self.cipher = AES.new(key, AES.MODE_CBC, iv=b'\x00' * 16)
+        self.key = key
+
+    def get_cipher(self) -> Any:
+        return AES.new(self.key, AES.MODE_CBC, iv=b'\x00' * 16)
 
     def encrypt(self, name: str) -> str:
-        return '/'.join(map(self.encrypt_one, name.split('/')))
+        cipher = self.get_cipher()
+        return '/'.join(map(lambda name : self.encrypt_one(name, cipher), name.split('/')))
 
-    def encrypt_one(self, name: str) -> str:
+    def encrypt_one(self, name: str, cipher: Any) -> str:
         name_bytes = name.encode('utf-8')
         name_bytes += b'\x00' * (16 - (len(name_bytes) % 16))
-        ciphertext = self.cipher.encrypt(name_bytes)
+        ciphertext = cipher.encrypt(name_bytes)
 
         return urlsafe_b64encode(ciphertext).decode('utf-8')
 
     def decrypt(self, name: str) -> str:
-        return '/'.join(map(self.decrypt_one, name.split('/')))
+        cipher = self.get_cipher()
+        return '/'.join(map(lambda name : self.decrypt_one(name, cipher), name.split('/')))
 
-    def decrypt_one(self, name: str) -> str:
-        ciphertext = self.cipher.decrypt(urlsafe_b64decode(name))
+    def decrypt_one(self, name: str, cipher: Any) -> str:
+        ciphertext = cipher.decrypt(urlsafe_b64decode(name))
 
         return ciphertext.decode('utf-8').rstrip('\x00')
