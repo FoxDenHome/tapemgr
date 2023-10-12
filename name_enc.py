6ffbd3a3d442a746de57abe299f6c09246295ffb
Doridian <git@doridian.net>
2023-10-11 23:20:03 -0700

    encrypt blank to blank

--- name_enc.py
+++ name_enc.py
@@ -12,9 +12,12 @@ class NameCryptor:
 
     def encrypt(self, name: str) -> str:
         cipher = self.get_cipher()
-        return '/'.join([self.encrypt_one(part, cipher) for part in name.split('/') if part])
+        return '/'.join([self.encrypt_one(part, cipher) for part in name.split('/')])
 
     def encrypt_one(self, name: str, cipher: Any) -> str:
+        if not name:
+            return ''
+
         name_bytes = name.encode('utf-8')
         name_bytes += b'\x00' * (16 - (len(name_bytes) % 16))
         ciphertext = cipher.encrypt(name_bytes)
@@ -23,9 +26,12 @@ class NameCryptor:
 
     def decrypt(self, name: str) -> str:
         cipher = self.get_cipher()
-        return '/'.join([self.decrypt_one(part, cipher) for part in name.split('/') if part])
+        return '/'.join([self.decrypt_one(part, cipher) for part in name.split('/')])
 
     def decrypt_one(self, name: str, cipher: Any) -> str:
+        if not name:
+            return ''
+
         ciphertext = cipher.decrypt(urlsafe_b64decode(name))
 
         return ciphertext.decode('utf-8').rstrip('\x00')
