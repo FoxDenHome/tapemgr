3341f445d63b378aa1e9839f59763c6ade7532a6
Doridian <git@doridian.net>
2023-10-11 21:55:28 -0700

    try adding crypto

--- name_enc.py
+++ name_enc.py
@@ -3,10 +3,16 @@ from Crypto.Cipher import AES
 
 def encrypt_filename(name: str, key: bytes) -> str:
     cipher = AES.new(key, AES.MODE_CBC, iv=b'\x00' * 16)
-    ciphertext = cipher.encrypt(name.encode('utf-8'))
+
+    name_bytes = name.encode('utf-8')
+    name_bytes += b'\x00' * (16 - (len(name_bytes) % 16))
+    ciphertext = cipher.encrypt(name_bytes)
+
     return urlsafe_b64encode(ciphertext).decode('utf-8')
 
 def decrypt_filename(name: str, key: bytes) -> str:
     cipher = AES.new(key, AES.MODE_CBC, iv=b'\x00' * 16)
+
     ciphertext = cipher.decrypt(urlsafe_b64decode(name))
+
     return ciphertext.decode('utf-8').rstrip('\x00')
