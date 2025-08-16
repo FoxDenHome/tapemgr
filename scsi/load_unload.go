62e776311c1665111a8916ed701e023372983542
Doridian <git@doridian.net>
2025-08-16 13:39:18 -0700

    Add LoadUnload

---
+++ scsi/load_unload.go
@@ -0,0 +1,22 @@
+package scsi
+
+type LoadUnloadOperation uint8
+
+const (
+	LOAD_AND_THREAD LoadUnloadOperation = 0b0001
+	LOAD_ONLY       LoadUnloadOperation = 0b1001
+	UNLOAD_FAST     LoadUnloadOperation = 0b0000
+	UNLOAD_ARCHIVE  LoadUnloadOperation = 0b0010
+)
+
+func (d *SCSIDevice) LoadUnload(op LoadUnloadOperation) error {
+	_, err := d.request([]byte{
+		LOAD_UNLOAD,
+		0x00, // Lowest bit is immediate, we want to always wait
+		0x00,
+		0x00,
+		uint8(op),
+		0x00,
+	}, 0)
+	return err
+}
