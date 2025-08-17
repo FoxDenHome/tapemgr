c28cc0895835abd05d490d35dd30487ce16c457e
Doridian <git@doridian.net>
2025-08-16 20:31:53 -0700

    Add file crypto

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/FoxDenHome/tapemgr
 
 go 1.24.6
 
-require github.com/platinasystems/scsi v0.0.0-20231015065257-70f6ba5c3b9c
+require (
+	filippo.io/age v1.2.1
+	github.com/platinasystems/scsi v0.0.0-20231015065257-70f6ba5c3b9c
+)
+
+require (
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+)
---
+++ storage/encryption/data.go
@@ -0,0 +1,87 @@
+package encryption
+
+import (
+	"io"
+	"os"
+
+	"filippo.io/age"
+)
+
+type FileCryptor struct {
+	identity  age.Identity
+	recipient age.Recipient
+}
+
+func NewFileCryptor(identityStr string) (*FileCryptor, error) {
+	identity, err := age.ParseX25519Identity(identityStr)
+	if err != nil {
+		return nil, err
+	}
+	return &FileCryptor{
+		identity:  identity,
+		recipient: identity.Recipient(),
+	}, nil
+}
+
+func NewFileCryptorEncryptOnly(recipientStr string) (*FileCryptor, error) {
+	recipient, err := age.ParseX25519Recipient(recipientStr)
+	if err != nil {
+		return nil, err
+	}
+	return &FileCryptor{
+		recipient: recipient,
+	}, nil
+}
+
+func (c *FileCryptor) Encrypt(dst io.Writer) (io.WriteCloser, error) {
+	return age.Encrypt(dst, c.recipient)
+}
+
+func (c *FileCryptor) EncryptFile(src string, dest string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	writer, err := c.Encrypt(destFile)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	_, err = io.Copy(writer, srcFile)
+	return err
+}
+
+func (c *FileCryptor) Decrypt(src io.Reader) (io.Reader, error) {
+	return age.Decrypt(src, c.identity)
+}
+
+func (c *FileCryptor) DecryptFile(src string, dest string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	reader, err := c.Decrypt(srcFile)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(destFile, reader)
+	return err
+}
