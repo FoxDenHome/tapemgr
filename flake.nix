97a7efd253943246a90248184c4831bc6c40a5d1
Doridian <git@doridian.net>
2025-10-14 09:59:18 -0700

    nix

--- flake.nix
+++ flake.nix
@@ -14,10 +14,38 @@
           vendorHash = "sha256-yLG9GbvJ/U8CS2rJgIy2aP4+Oj386HQ6TpmlGWVqrpg=";
           buildInputs = [];
         };
+
+        ltfs = pkgs.stdenv.mkDerivation rec {
+          pname = "ltfs";
+          version = "1.0.0";
+
+          src = pkgs.fetchFromGitHub {
+            rev = "9d232e551628d954c41497a45aedf2315ffca591";
+            owner = "LinearTapeFileSystem";
+            repo = "ltfs";
+            sha256 = "193593hsc8nf5dn1fkxhzs1z4fpjh64hdkc8q6n9fgplrpxdlr4s";
+          };
+
+          sourceRoot = "${src.name}/ltfs";
+
+          # include sys/sysctl.h is deprecated in glibc. The sysctl calls are only used
+          # for Apple to determine the kernel version. Because this build only targets
+          # Linux is it safe to remove.
+          patches = [ ./remove-sysctl.patch ];
+
+          nativeBuildInputs = [ pkgs.pkg-config ];
+
+          buildInputs = with pkgs; [
+            fuse
+            icu66
+            libxml2
+            libuuid
+          ];
+        };
       in
       {
         packages = {
-          inherit tapemgr;
+          inherit tapemgr ltfs;
           default = tapemgr;
         };
       });
